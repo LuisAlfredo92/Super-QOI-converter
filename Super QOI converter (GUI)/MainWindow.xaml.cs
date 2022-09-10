43e03e802b4ab4d77f691d587f309d80735e61ac
Luis Alfredo Figueroa Bracamontes <92luisalfredo@protonmail.com>
2022-09-09 19:04:20 -0500

    Creating UI

--- Super QOI converter (GUI)/MainWindow.xaml.cs
+++ Super QOI converter (GUI)/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Super_QOI_converter__GUI_
 {
@@ -20,9 +22,53 @@ namespace Super_QOI_converter__GUI_
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public class FileRow
+        {
+            private Image StateImage { get; set; }
+            private string Path { get; set; }
+            private Button DeleteButton { get; set; } 
+
+
+            public FileRow(string path)
+            {
+                StateImage = new()
+                {
+                    MaxHeight = 10,
+                    MaxWidth = 10,
+                    Height = 8,
+                    Width = 8,
+                };
+                Path = path;
+                DeleteButton = new()
+                {
+                    Width = 10,
+                    Content = "Delete"
+                };
+            }
+        }
+
+        private List<FileRow> files;
+
         public MainWindow()
         {
             InitializeComponent();
+            files = new();
+        }
+
+        private void AddFilesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Multiselect = true,
+                Title = "Select files to be added",
+                Filter = "Supported images|*.jpeg;*.png;*.jpg;*.bmp;*.JPEG;*.PNG;*.JPG;*.BMP"
+            };
+            openFileDialog.ShowDialog();
+            foreach (var file in openFileDialog.FileNames)
+                files.Add(new FileRow(file));
+
+            FilesListView.ItemsSource = files;
         }
     }
 }
